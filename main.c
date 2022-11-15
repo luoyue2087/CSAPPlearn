b76cfcc1cdd14c78e149c681f81afd603cccf727
luoyue <2087682597@qq.com>
2022-11-15 19:57:44 +0800

    初始化 深入理解计算机系统书籍阅读

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+void md(){
+    for(int i=61; i<=97; ++i){
+        printf("### 2.%d 未完成\n", i);
+        printf(">\n");
+        printf("```cpp\n```\n\n");
+    }
+}
+
+int main(){
+    // md();
+    return 0;
+}
+//g++ main.c -o a && ./a
