1f793ebfc69077e31fd535367ff4cdfd4e1a230e
luoyue <2087682597@qq.com>
2023-02-14 15:54:53 +0800

    日常提交

--- main.c
+++ main.c
@@ -1,31 +1,9 @@
-#include <iostream>
-#include <bitset>
-#include <assert.h>
-using namespace std;
-
-void fun2_59(){
-    size_t mask = 0xff;
-    size_t x = 0x89ABCDEF;
-    size_t y = 0x76543210;
-    size_t res = (x & mask) | (y & ~mask);
-    assert(res == 0x765432EF);
+void gets(char*);
+void puts(char*);
+void echo(){
+    char buf[8];
+    gets(buf);
+    puts(buf);
 }
-
-//移动一个字节在二进制中是移动八位！
-unsigned replace_byte (unsigned x, int i, unsigned char b){
-    unsigned char mask = 0xff;
-    x = x & (~(mask << i*8));
-    x = x | (b<<(i*8));
-
-    return x;
-}
-
-
-int main(){
-    // fun2_59();
-    // printf("%X\n",replace_byte(0x12345678,2,0xAB));
-    // printf("%X\n",replace_byte(0x12345678,0,0xAB));
-    
-    return 0;
-}
-//g++ main.c -o a && ./a
+//g++ -S main.c -Og
+//g++ -o prog main.c
